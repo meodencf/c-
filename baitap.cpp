5d9e42a594e91c3ee28257e7034a4ee31b6ee4e6
meodencf <125757693+meodencf@users.noreply.github.com>
2023-02-20 00:09:43 +0700

    all

--- baitap.cpp
+++ baitap.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct THANHPHO {
     int maTP;
-    char tenTP[31];
+    string tenTP;
     float dienTich;
 };
-// Ham nhap du lieu cho mot thanh pho
-void nhapThanhPho(THANHPHO& tp) {
+
+int main() {
+    THANHPHO tp;
     cout << "Nhap ma thanh pho: ";
     cin >> tp.maTP;
-    cin.ignore(); // bo di ky tu xuong dong sau khi nhap so nguyen
     cout << "Nhap ten thanh pho: ";
-    cin.getline(tp.tenTP, 31);
+    getline(cin, tp.tenTP);
     cout << "Nhap dien tich: ";
     cin >> tp.dienTich;
-}
-// Ham xuat thong tin cua mot thanh pho
-void xuatThanhPho(THANHPHO tp) {
+
+    cout << endl << "Thong tin thanh pho vua nhap:" << endl;
     cout << "Ma thanh pho: " << tp.maTP << endl;
     cout << "Ten thanh pho: " << tp.tenTP << endl;
     cout << "Dien tich: " << tp.dienTich << endl;
-}
-int main() {
-    THANHPHO tp;
-    nhapThanhPho(tp);
-    xuatThanhPho(tp);
+
     return 0;
 }
-
